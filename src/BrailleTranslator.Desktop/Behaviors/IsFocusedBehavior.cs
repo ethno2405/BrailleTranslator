b479e15b124873dade66bc157cd0fd43e63979a8
konstantin <k.tenekedzhiev@gmail.com>
2015-08-29 01:48:59 +0300

    IsFocusedBehavior implemented
    -Unable to bind IsFocused property to the behavior.

--- src/BrailleTranslator.Desktop/Behaviors/IsFocusedBehavior.cs
+++ src/BrailleTranslator.Desktop/Behaviors/IsFocusedBehavior.cs
@@ -5,6 +5,20 @@ namespace BrailleTranslator.Desktop.Behaviors
 {
     public class IsFocusedBehavior : Behavior<UIElement>
     {
+        public static readonly DependencyProperty IsFocusedProperty = DependencyProperty.RegisterAttached("IsFocused", typeof(bool), typeof(IsFocusedBehavior), new PropertyMetadata(OnIsFocusedChanged));
+
+        public bool IsFocused
+        {
+            get
+            {
+                return (bool)GetValue(IsFocusedProperty);
+            }
+            set
+            {
+                SetValue(IsFocusedProperty, value);
+            }
+        }
+
         protected override void OnAttached()
         {
             base.OnAttached();
@@ -13,14 +27,34 @@ namespace BrailleTranslator.Desktop.Behaviors
             AssociatedObject.LostFocus += OnAssociatedObjectLostFocus;
         }
 
+        protected override void OnDetaching()
+        {
+            AssociatedObject.GotFocus -= OnAssociatedObjectGotFocus;
+            AssociatedObject.LostFocus -= OnAssociatedObjectLostFocus;
+
+            base.OnDetaching();
+        }
+
+        private static void OnIsFocusedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as IsFocusedBehavior;
+
+            if (behavior == null) return;
+
+            if ((bool)e.NewValue)
+            {
+                behavior.AssociatedObject.Focus();
+            }
+        }
+
         private void OnAssociatedObjectLostFocus(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            IsFocused = false;
         }
 
         private void OnAssociatedObjectGotFocus(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            IsFocused = true;
         }
     }
 }
