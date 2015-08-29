b9427fef7770a30d68e2db1e03ce3502c7c33fcb
DRJ <d.jovanovic@televic.com>
2015-08-29 13:12:12 +0300

    Ope, Save, Print and Exit dialogs - added.

--- src/BrailleTranslator.Desktop/ViewModels/ToolbarViewModel.cs
+++ src/BrailleTranslator.Desktop/ViewModels/ToolbarViewModel.cs
@@ -1,8 +1,78 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+using System;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace BrailleTranslator.Desktop.ViewModels
 {
     public class ToolbarViewModel : ViewModelBase
     {
+        public static ICommand OpenCommand { get; set; }
+        public static ICommand SaveCommand { get; set; }
+        public static ICommand PrintCommand { get; set; }
+        public static ICommand ExitCommand { get; set; }
+
+        private string _selectedPath;
+        public string SelectedPath
+        {
+            get { return _selectedPath; }
+            set
+            {
+                _selectedPath = value;
+                RaisePropertyChanged("SelectedPath");
+            }
+        }
+
+        private string _defaultPath;
+
+        public ToolbarViewModel()
+        {
+            RegisterCommands();
+        }
+        /*
+        public ToolbarViewModel(string defaultPath)
+        {
+            _defaultPath = defaultPath;
+            RegisterCommands();
+        }*/
+
+        private void RegisterCommands()
+        {
+            OpenCommand = new RelayCommand(ExecuteOpenFileDialog);
+            SaveCommand = new RelayCommand(ExecuteSaveFileDialog);
+            PrintCommand = new RelayCommand(ExecutePrintFileDialog);
+            ExitCommand = new RelayCommand(ExecuteExitDialog);
+        }
+
+        private void ExecuteOpenFileDialog()
+        {
+            var dialog = new OpenFileDialog { InitialDirectory = _defaultPath };
+            dialog.ShowDialog();
+
+            SelectedPath = dialog.FileName;
+        }
+
+        private void ExecuteSaveFileDialog()
+        {
+            var dialog = new SaveFileDialog { InitialDirectory = _defaultPath };
+            dialog.ShowDialog();
+
+            SelectedPath = dialog.FileName;
+        }
+
+        private void ExecutePrintFileDialog()
+        {
+            var dialog = new PrintDialog();
+            dialog.ShowDialog();
+        }
+
+        private void ExecuteExitDialog()
+        {
+            Environment.Exit(0);
+        }
+
+
     }
 }
--- src/BrailleTranslator.Desktop/Views/ToolbarView.xaml.cs
+++ src/BrailleTranslator.Desktop/Views/ToolbarView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrailleTranslator.Desktop.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace BrailleTranslator.Desktop.Views
     {
         public ToolbarView()
         {
+            DataContext = new ToolbarViewModel();
             InitializeComponent();
         }
     }
